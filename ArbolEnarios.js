a1b84a4855804f63498fbc94e71a8c860dd63c5c
unknown <alejandrojzeballos@gmail.com>
2021-05-02 21:16:13 -0400

    {add} arbol enario de un nivel

--- ArbolEnarios.js
+++ ArbolEnarios.js
@@ -58,5 +58,26 @@ class Nodo{
     }
 }
 
+const nodoPadre = new Nodo("Padre",null, 0);
+const arbol = new Tree(nodoPadre);
+
+let longitudArray = arrayObjetosPrueba.length;
+
+for (let i = 0; i < longitudArray; i++) {
+    level = 0;
+    auxNodo = new Nodo(arrayObjetosPrueba[i].Token, null ,level);
+    nodoPadre.añadirHijo(auxNodo);
+    arrayHijo = arrayObjetosPrueba[i].Value;
+    recorrerArrayValues( arrayHijo, level, arrayObjetosPrueba[i].Token ,auxNodo );
+};
+
+function recorrerArrayValues( arrayHijo, level, Token, nodo ){
+    level++;
+    arrayHijo.forEach(data=>{
+        auxNodo = new Nodo(Token,data,level);
+        nodo.añadirHijo(auxNodo);
+    });
+};
 
 
+console.log(arbol);
